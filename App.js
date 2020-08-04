c7b33acaf8f5c814429764ad9a2df99d410fe797
akintomiwa fisayo <akintomiwa.fisayo@gmail.com>
2020-08-05 00:24:16 +0100

    update

--- App.js
+++ App.js
@@ -6,14 +6,14 @@ import { AppLoading } from 'expo';
 import * as firebase from 'firebase'
 
 var firebaseConfig = {
-  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  projectId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  measurementId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  apiKey: "AIzaSyCgk2I3xIWYSd5LFa-bAR7651DlB_KUe08",
+  authDomain: "logis-ef54f.firebaseapp.com",
+  databaseURL: "https://logis-ef54f.firebaseio.com",
+  projectId: "logis-ef54f",
+  storageBucket: "logis-ef54f.appspot.com",
+  messagingSenderId: "980070107115",
+  appId: "1:980070107115:web:14d5eb8f9436fab67b7bae",
+  measurementId: "G-KPM1YYDPL3"
 };
 
 
